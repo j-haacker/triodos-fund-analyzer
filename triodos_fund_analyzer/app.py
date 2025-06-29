c245c6d08f6b735a244d4fdcefda6bf09a5b68d6
jan <152862650+j-haacker@users.noreply.github.com>
2025-06-29 15:58:36 +0200

    Update app

--- triodos_fund_analyzer/app.py
+++ triodos_fund_analyzer/app.py
@@ -7,6 +7,20 @@ import triodos_fund_analyzer as tfa
 from triodos_fund_analyzer.funds import details_df
 
 
+ethics_statement = """\
+Holding shares usually is not ethical for a number of reasons. Triodos
+funds are among the rare exceptions that have less unethical impact.
+
+In general it is not ethical to make profits of other peoples work. Even
+more so, because for the largest part the unequal distribution of wealth
+requires the poor to borrow money for their causes.
+
+It may be ethical if you give the profits of fund investments to
+[objectively](https://www.britannica.com/topic/effective-altruism)
+[good](https://plato.stanford.edu/entries/common-good/) projects.
+"""
+
+
 def build_page():
     st.title("Triodos Fund Analyzer")
     fig, ax = plt.subplots()
@@ -28,11 +42,18 @@ def build_page():
     for fund in st.session_state["funds"]:
         tfa.plot_history(fund, ax=ax, **options)
     ax.set_xlabel("")
+    ax.set_ylabel(
+        f"Normalized value ({options['normalize']})"
+        if options["normalize"]
+        else "Share revenue, €"
+    )
     ax.legend()
     fig.tight_layout()
     plt.ion()
     plt.show()
     st.pyplot(fig=fig, clear_figure=True, use_container_width=True)
+    st.subheader("Moral considerations")
+    st.markdown(ethics_statement)
 
 
 if __name__ == "__main__":
@@ -40,25 +61,35 @@ if __name__ == "__main__":
         "Funds",
         details_df.index.values,
         selection_mode="multi",
+        default=["TGF", "TFSF"],
         key="funds",
         help=details_df.full_name.to_string(),
     )
-    st.sidebar.slider("normalize", 0, 100, 100, key="normalize")
+    st.sidebar.date_input(
+        "Start date", "1990-01-01", "1990-01-01", "today", key="start_date"
+    )
     st.sidebar.segmented_control(
-        "purchasing power",
+        "Purchasing power",
         ["off", "CPI", "HICP"],
         default="CPI",
         help="Choose index to account for inflation",
         key="purchasing_power",
     )
+    st.sidebar.slider(
+        "Normalize",
+        0,
+        100,
+        100,
+        key="normalize",
+        help="Show normalized values instead of trading price. Set 0 to disable normalization.",
+    )
     st.sidebar.toggle(
-        "deduce costs",
+        "Deduce costs",
         True,
         help="Reduce fund value by fund costs and service fees",
         key="deduce_costs",
     )
-    st.sidebar.toggle("consider dividends", True, key="consider_dividends")
-    st.sidebar.date_input("start date", "1990-01-01", key="start_date")
-    st.sidebar.toggle("add baseline", True, key="add_baseline")
+    st.sidebar.toggle("Consider dividends", True, key="consider_dividends")
+    st.sidebar.toggle("Add baseline", True, key="add_baseline")
 
     build_page()
